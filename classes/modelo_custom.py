b222aad97dcab48694c0a9ce9a61a2c2220cc3ab
Vinicius Giovanini <vgiovanini@sga.pucminas.br>
2024-11-25 20:55:45 -0300

    doc: Corrigindo o código e comentando (Falta testar)

--- classes/modelo_custom.py
+++ classes/modelo_custom.py
@@ -18,158 +18,174 @@ class CustomPatchEmbedding(nn.Module):
         self.embed_dim = embed_dim
         self.num_patches = num_patches
         self.is_visualizer = is_visualizer
-        
+
         # Projeta os patches para uma dimensão `embed_dim`
         self.projection = nn.Linear(patch_size[0] * patch_size[1] * input_size[0], embed_dim)
         # Lida com a visualizacao de patches
-        self.visualizer = PatchVisualizer(patch_size)
+        # self.visualizer = PatchVisualizer(patch_size)
+
         
-        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        
+        #########################################################################
+        # Descomente a linha que será carregado o centros de determinados métodos
+        #########################################################################
         
         # Arquivo dos centros randomicos melhorados
         # self.dict_center = self.load_dict('./data/centros_pre_salvos/randomico_melhorado_identificador_por_imgname.pkl')
-        
+
         # Arquivo dos centros segmentados
-        self.dict_center = self.load_dict('./data/centros_pre_salvos/segmentacao_dicionario.pkl')
-        
-        
-    
+        # self.dict_center = self.load_dict('./data/centros_pre_salvos/segmentacao_dicionario.pkl')
+
+        # Arquivo dos centros segmentados 32x32 de patch_size
+        # self.dict_center = self.load_dict('./data/centros_pre_salvos/segmentacao_dicionario_32_patch_size.pkl')
+
+
+
     def load_dict(self, path):
       with open(path, 'rb') as f:
         lista_centro_dict  = pickle.load(f)
-        
+
       return lista_centro_dict
-    
+
     def forward(self, x, **kwargs):
-        
+
         # X -> Tensor de entrada (batch_size, channels, height, width)
 
         batch_size, channels, height, width = x.size()
-        
+
         # armazena patches extraidos
         all_patches = []
-        
+
         # armazena os índices dos centros dos patches
         all_h_indices = []
         all_w_indices = []
 
-        
+
         ################################################
         #                   Print de Log               #
         ################################################
         # print("Iniciou um loop de batch\n")
         # print(f"Printando de dentro do CustomPatchEmbedding: {image_names_dict}")
-        
-        
-        
+
+
+
         # Loop sobre cada img do batch
         for b in range(batch_size):
             # Seleciona os centros de acordo com o metodo escolhido
             # centers = DynamicPatches().generate_patch_centers(height, width, self.patch_size)
-            # centers = DynamicPatches().generate_random_patch_centers(height, width, self.patch_size, self.num_patches)
+            
+            
+            
+            ############################################################
+            #              Descomentar caso use aboradagem SR          #
+            ############################################################
+            
+            centers = DynamicPatches().generate_random_patch_centers(height, width, self.patch_size, self.num_patches)
             # centers = DynamicPatches().random_patchs_melhorados(self.patch_size, self.num_patches, x[b])
             # centers = DynamicPatches().grabcutextractcenters(imagem_tensor=x[b], tamanho_img=(height, width), stride=self.patch_size[0])
             # centers = DynamicPatches().random_patchs_melhorados(self.patch_size, self.num_patches, x[b])
+
             
-            try:
-              centers = self.dict_center[image_names_dict[b]]
-            except:
-              print(f"Erro ao encontrar centro --> {image_names_dict[b]}")
-              
-                    
-            # converter as cordernadas do centers em indices inteiros  
+            ############################################################
+            #              Descomentar caso use aboradagem SS e RA     #
+            ############################################################
+            # try:
+            #   centers = self.dict_center[image_names_dict[b]]
+            # except:
+            #   print(f"Erro ao encontrar centro --> {image_names_dict[b]}")
+
+
+            # converter as cordernadas do centers em indices inteiros
             h_indices = [int(h) for h, _ in centers]
             w_indices = [int(w) for _, w in centers]
-            
+
             patches = []
 
             # Para cada par ded indices h,w
             for (h_idx, w_idx) in zip(h_indices, w_indices):
-              
+
                 # Calcular as coordenadas de início do patch
                 start_h = h_idx - self.patch_size[0] // 2
                 start_w = w_idx - self.patch_size[1] // 2
-                
-                
+
+
                 # Calcular as coordenadas de fim do patch
                 end_h = start_h + self.patch_size[0]
                 end_w = start_w + self.patch_size[1]
-              
+
                 # Verificar se o patch está dentro dos limites da imagem
                 if (0 <= start_h and start_h + self.patch_size[0] <= height and
                     0 <= start_w and start_w + self.patch_size[1] <= width):
-                    
+
                     # Extrair o patch
                     patch = x[b, :, start_h:end_h, start_w:end_w]
                     patches.append(patch.to(device))
                 else:
                     print(f"Patch fora dos limites: start_h={start_h}, end_h={end_h}, start_w={start_w}, end_w={end_w}")
-            
-            
-            # print(f"Quantidade de patches extraidos: {len(patches)}")
-            
+
+
+
             # se o numero patches for menor que o ncessário, prenche com tesnores vazios
             if len(patches) < self.num_patches:
                 print("ERRO: Gerando patch preto\n\n\n\n\n\n\n\n\n")
                 missing_patches = self.num_patches - len(patches)
                 patches += [torch.zeros(channels, self.patch_size[0], self.patch_size[1], device=device)] * missing_patches
 
-            # print(f"Quantidade de patches extraidos: {len(patches)}")
-            
+
             ##################################
             # Visualização do Patch Tensor
             ##################################
-            if self.is_visualizer:
+            # if self.is_visualizer:
               # self.visualizer.visualize_patches_with_tensor(patches)
-              self.visualizer.visualize_patch_centers(x[b], centers, self.patch_size, image_names_dict[b])              
-            
+              # self.visualizer.visualize_patch_centers(x[b], centers, self.patch_size, image_names_dict[b])
+
             # Concatena os patches em um unico tensor
-            patches = torch.stack(patches)  
-            # print(f"Shape depois de empilhar patches: {patches.shape}")
+            patches = torch.stack(patches)
 
             # faz o flatten
             patches = patches.flatten(start_dim=1)
-            # print(f"Shape depois do flattening patches: {patches.shape}")
-            
+
             # projeta os patches para um espaço de maior dimensão
-            patches = self.projection(patches)    
-            # print(f"Shape depois da projection patches: {patches.shape}")
-            
+            patches = self.projection(patches)
+
             all_patches.append(patches)
             all_h_indices.append(h_indices)
             all_w_indices.append(w_indices)
-        
-        
+
+
         # combina todos os patches de todas as imagens no batch em um uico tensor tridimensional (batch_size, num_patches, embed_dim)
-        all_patches = torch.stack(all_patches).to(device) 
+        all_patches = torch.stack(all_patches).to(device)
+
         return all_patches
-           
-      
+
+
 class ModeloCustom(pl.LightningModule):
     def __init__(self, num_class, learning_rate, num_patch, input_size, patch_size, batch_size):
         super(ModeloCustom, self).__init__()
-        
+
         self.save_hyperparameters()
-        
+
         self.num_class = num_class
         self.learning_rate = learning_rate
         self.layer_dropout = nn.Dropout(0.4)
         self.batch_size = batch_size
-      
+
         # Carregar um modelo pré-treinado
         base_model = ViTModel.from_pretrained('google/vit-base-patch16-224')
-        
+        # base_model = ViTModel.from_pretrained('google/vit-base-patch32-224-in21k')
+        # base_model = ViTModel.from_pretrained('WinKawaks/vit-small-patch16-224')
+
         self.model = ViTForImageClassification(config=base_model.config)
         self.model.vit = base_model
-        
+
         self.model.vit.embeddings.patch_embeddings = CustomPatchEmbedding(
-            input_size=(3, input_size, input_size),  
-            patch_size=patch_size,       
+            input_size=(3, input_size, input_size),
+            patch_size=patch_size,
             embed_dim=self.model.config.hidden_size,
             num_patches=num_patch,
             is_visualizer=True,
         )
-              
+
         print(self.model)
         self.model.to(device)
         print("----------------------------------------------------------------")
@@ -195,7 +211,7 @@ class ModeloCustom(pl.LightningModule):
                 "vit.pooler"
             ]):
                 param.requires_grad = True
-        
+
         self.model.classifier = nn.Sequential(
             nn.Linear(self.model.config.hidden_size, self.model.config.hidden_size),
             nn.ReLU(),
@@ -219,15 +235,15 @@ class ModeloCustom(pl.LightningModule):
     # Retorna os logits para passar na funcao softmax
     def forward(self, x, validation_mode = False, img_names_validation=None):
 
-        
+
       if validation_mode:
         global image_names_dict
-      
+
         image_names_dict.clear()
-        
+
         for i, name in enumerate(img_names_validation):
           image_names_dict[i] = name
-        
+
       logits = self.model(x).logits
       return logits
 
@@ -240,14 +256,14 @@ class ModeloCustom(pl.LightningModule):
         images, labels = images.to(device), labels.to(device)
 
         global image_names_dict
-        
+
         image_names_dict.clear()
-        
+
         for i, name in enumerate(img_names):
           image_names_dict[i] = name
-        
+
         # Obte os logits passando as imagens através do foward
-        logits = self(images) 
+        logits = self(images)
 
         # Calcula a perda
         loss = self.criterion(logits, labels)
@@ -269,15 +285,15 @@ class ModeloCustom(pl.LightningModule):
     def validation_step(self, batch):
         images, labels, img_names = batch
         images, labels = images.to(device), labels.to(device)
-        
+
         global image_names_dict
-        
+
         image_names_dict.clear()
-        
+
         for i, name in enumerate(img_names):
           image_names_dict[i] = name
-        
-        logits = self(images) 
+
+        logits = self(images)
         loss = self.criterion(logits, labels)
         _, predicted = torch.max(logits, 1)
         accuracy = (predicted == labels).float().mean()
--- classes/modelo_custom_conv2d.py
+++ classes/modelo_custom_conv2d.py
@@ -18,91 +18,92 @@ class CustomPatchEmbedding(nn.Module):
         self.embed_dim = embed_dim
         self.num_patches = num_patches
         self.is_visualizer = is_visualizer
-        
-        
+
         # Projeta os patches para uma dimensão `embed_dim`
         # self.projection = nn.Linear(patch_size[0] * patch_size[1] * input_size[0], embed_dim)
-        
-        
+
+
         self.projection = nn.Conv2d(
             in_channels=input_size[0],   # Número de canais da imagem de entrada
             out_channels=embed_dim,       # Dimensão de saída do embedding
             kernel_size=patch_size[0],       # Tamanho do patch
             stride=patch_size[0]             # Avanço igual ao tamanho do patch
         )
-        
-        
-        
+
+
+
         # Lida com a visualizacao de patches
         # self.visualizer = PatchVisualizer(patch_size)
-        
+
         # Arquivo dos centros randomicos melhorados
         # self.dict_center = self.load_dict('./data/centros_pre_salvos/randomico_melhorado_identificador_por_imgname.pkl')
-        
+
         # Arquivo dos centros segmentados
-        self.dict_center = self.load_dict('./data/centros_pre_salvos/segmentacao_dicionario.pkl')
-        
-        
-    
+        # self.dict_center = self.load_dict('./data/centros_pre_salvos/segmentacao_dicionario.pkl')
+
+
+
     def load_dict(self, path):
       with open(path, 'rb') as f:
         lista_centro_dict  = pickle.load(f)
-        
+
       return lista_centro_dict
-    
+
     def forward(self, x, **kwargs):
-        
+
         # X -> Tensor de entrada (batch_size, channels, height, width)
 
         batch_size, channels, height, width = x.size()
-        
+
         # armazena patches extraidos
         all_patches = []
-        
+
         # armazena os índices dos centros dos patches
         all_h_indices = []
         all_w_indices = []
 
-        
+
         ################################################
         #                   Print de Log               #
         ################################################
         # print("Iniciou um loop de batch\n")
         # print(f"Printando de dentro do CustomPatchEmbedding: {image_names_dict}")
-        
+
         each_image = {}
-        
+
         # Loop sobre cada img do batch
         for b in range(batch_size):
             # Seleciona os centros de acordo com o metodo escolhido
-            
-            try:
-              centers = self.dict_center[image_names_dict[b]]
-            except:
-              print(f"Erro ao encontrar centro --> {image_names_dict[b]}")
-                    
-            # converter as cordernadas do centers em indices inteiros  
+
+            # centers = DynamicPatches().generate_patch_centers(height, width, self.patch_size)
+            centers = DynamicPatches().generate_random_patch_centers(height, width, self.patch_size, self.num_patches)
+            # try:
+            #   centers = self.dict_center[image_names_dict[b]]
+            # except:
+            #   print(f"Erro ao encontrar centro --> {image_names_dict[b]}")
+
+            # converter as cordernadas do centers em indices inteiros
             h_indices = [int(h) for h, _ in centers]
             w_indices = [int(w) for _, w in centers]
-            
+
             patches = []
 
             # Para cada par ded indices h,w
             for (h_idx, w_idx) in zip(h_indices, w_indices):
-              
+
                 # Calcular as coordenadas de início do patch
                 start_h = h_idx - self.patch_size[0] // 2
                 start_w = w_idx - self.patch_size[1] // 2
-                
-                
+
+
                 # Calcular as coordenadas de fim do patch
                 end_h = start_h + self.patch_size[0]
                 end_w = start_w + self.patch_size[1]
-              
+
                 # Verificar se o patch está dentro dos limites da imagem
                 if (0 <= start_h and start_h + self.patch_size[0] <= height and
                     0 <= start_w and start_w + self.patch_size[1] <= width):
-                    
+
                     # Extrair o patch
                     patch = x[b, :, start_h:end_h, start_w:end_w]
                     # print(f'Shape antes da conv: {patch.shape}')
@@ -111,59 +112,60 @@ class CustomPatchEmbedding(nn.Module):
 
                     # fazendo o flatten
                     patches.append(output_patches.view(-1))
-                    
+
                     # patches.append(output_patches)
                 else:
                     print(f"Patch fora dos limites: start_h={start_h}, end_h={end_h}, start_w={start_w}, end_w={end_w}")
             # a = torch.stack(patches)
             # each_image[image_name[b]] = torch.stack(patches)
-            
+
             each_image[image_names_dict[b]] = torch.stack(patches)
-            
+
             # print(f"Shape do tensor da imagem {image_name[b]} é {each_image[image_name[b]].shape}")
             # print(f"Shape de um patch: {patches[0].shape}")
             # print(f"Shape de todos os patches empilhados: {each_image[image_key].shape}")
             # str("")
-        
-        
-        
+
+
+
         all_images = torch.stack(list(each_image.values()))  # Shape: [batch_size, num_patches, output_channels]
         # print(f"Forma final das imagens: {all_images.shape}")
         return all_images
-           
-      
+
+
 class ModeloCustomConv2d(pl.LightningModule):
     def __init__(self, num_class, learning_rate, num_patch, input_size, patch_size, batch_size):
         super(ModeloCustomConv2d, self).__init__()
-        
+
         self.save_hyperparameters()
-        
+
         self.num_class = num_class
         self.learning_rate = learning_rate
         self.layer_dropout = nn.Dropout(0.4)
         self.batch_size = batch_size
-      
+
         # Carregar um modelo pré-treinado
-        base_model = ViTModel.from_pretrained('google/vit-base-patch16-224')
+        # base_model = ViTModel.from_pretrained('google/vit-base-patch16-224')
+        base_model = ViTModel.from_pretrained('WinKawaks/vit-small-patch16-224')
         # base_model = ViTModel.from_pretrained('google/vit-base-patch32-224-in21k')
-        
+
         # Acessar os pesos da camada Conv2d da projeção do modelo base
         pretrained_conv_weights = base_model.embeddings.patch_embeddings.projection.weight.data.clone()
         pretrained_conv_bias = base_model.embeddings.patch_embeddings.projection.bias.data.clone()
-        
+
         self.model = ViTForImageClassification(config=base_model.config)
         self.model.vit = base_model
-        
+
         self.model.vit.embeddings.patch_embeddings = CustomPatchEmbedding(
-            input_size=(3, input_size, input_size),  
-            patch_size=patch_size,       
+            input_size=(3, input_size, input_size),
+            patch_size=patch_size,
             embed_dim=self.model.config.hidden_size,
             num_patches=num_patch,
             is_visualizer=True,
         )
-              
-        
-        
+
+
+
         # Copiar os pesos e bias para a sua camada personalizada
         self.model.vit.embeddings.patch_embeddings.projection.weight.data.copy_(pretrained_conv_weights)
         self.model.vit.embeddings.patch_embeddings.projection.bias.data.copy_(pretrained_conv_bias)
@@ -171,11 +173,11 @@ class ModeloCustomConv2d(pl.LightningModule):
         print(self.model)
         self.model.to(device)
         print("----------------------------------------------------------------")
-        # print(f'Weight data: {self.model.vit.embeddings.patch_embeddings.projection.weight.data} \n\n\n\n\n\n\n')  
+        # print(f'Weight data: {self.model.vit.embeddings.patch_embeddings.projection.weight.data} \n\n\n\n\n\n\n')
 
 
 
-        
+
 
         # Congela todos os parametros
         for param in self.model.parameters():
@@ -189,14 +191,23 @@ class ModeloCustomConv2d(pl.LightningModule):
         for name, param in self.model.named_parameters():
             if any(layer_name in name for layer_name in [
                 "vit.embeddings.patch_embeddings.projection",
-                "vit.encoder.layer.11.intermediate",
-                "vit.encoder.layer.11.output",
-                "vit.encoder.layer.11.layernorm",
+                "vit.encoder.layer.1.",
+                "vit.encoder.layer.2.",
+                "vit.encoder.layer.9.",
+                "vit.encoder.layer.10.",
+                "vit.encoder.layer.11.",
                 "vit.layernorm",
                 "vit.pooler"
             ]):
                 param.requires_grad = True
-        
+
+        # Adicionando Regularização
+        self.model.vit.encoder.layer[1].output.dropout = self.layer_dropout
+        self.model.vit.encoder.layer[2].output.dropout = self.layer_dropout
+        # self.model.vit.encoder.layer[10].attention.output.dropout = self.layer_dropout
+        # self.model.vit.encoder.layer[11].attention.attention.dropout = self.layer_dropout
+
+        # self.model.classifier = torch.nn.Linear(base_model.config.hidden_size, self.num_class)
         self.model.classifier = nn.Sequential(
             nn.Linear(self.model.config.hidden_size, self.model.config.hidden_size),
             nn.ReLU(),
@@ -220,15 +231,15 @@ class ModeloCustomConv2d(pl.LightningModule):
     # Retorna os logits para passar na funcao softmax
     def forward(self, x, validation_mode = False, img_names_validation=None):
 
-        
+
       if validation_mode:
         global image_names_dict
-      
+
         image_names_dict.clear()
-        
+
         for i, name in enumerate(img_names_validation):
           image_names_dict[i] = name
-        
+
       logits = self.model(x).logits
       return logits
 
@@ -241,14 +252,14 @@ class ModeloCustomConv2d(pl.LightningModule):
         images, labels = images.to(device), labels.to(device)
 
         global image_names_dict
-        
+
         image_names_dict.clear()
-        
+
         for i, name in enumerate(img_names):
           image_names_dict[i] = name
-        
+
         # Obte os logits passando as imagens através do foward
-        logits = self(images) 
+        logits = self(images)
 
         # Calcula a perda
         loss = self.criterion(logits, labels)
@@ -270,15 +281,15 @@ class ModeloCustomConv2d(pl.LightningModule):
     def validation_step(self, batch):
         images, labels, img_names = batch
         images, labels = images.to(device), labels.to(device)
-        
+
         global image_names_dict
-        
+
         image_names_dict.clear()
-        
+
         for i, name in enumerate(img_names):
           image_names_dict[i] = name
-        
-        logits = self(images) 
+
+        logits = self(images)
         loss = self.criterion(logits, labels)
         _, predicted = torch.max(logits, 1)
         accuracy = (predicted == labels).float().mean()
